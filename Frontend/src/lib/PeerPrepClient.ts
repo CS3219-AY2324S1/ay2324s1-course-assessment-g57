47339c7a0f55f7cad750ef3fc3f866d57ffa2d2f
He Shuimei <shuimeihe@gmail.com>
2023-09-19 03:14:51 +0800

    Add PeerPrepClient.ts

---
+++ Frontend/src/lib/PeerPrepClient.ts
@@ -0,0 +1,91 @@
+import { User, Question } from "../models/types";
+
+export class PeerPrepClient {
+    
+    private readonly baseURL: string;
+
+    constructor(baseURL = "http://localhost:4000") {
+        this.baseURL = baseURL;
+    }
+
+    public async getUser(id: number): Promise<User> {
+        const response = await fetch(`${this.baseURL}/users/${id}`);
+        return await response.json();   
+    }
+
+    public async getUsers(): Promise<Array<User>> {
+        const response = await fetch(`${this.baseURL}/users`);
+        return await response.json();
+    }
+
+    public async createUser(user: User): Promise<User> {
+        const response = await fetch(`${this.baseURL}/users`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        });
+        return await response.json();
+    }
+
+    public async updateUser(user: User): Promise<User> {
+        const response = await fetch(`${this.baseURL}/users/${user.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        });
+        return await response.json();
+    }
+
+    public async deleteUser(id: number): Promise<void> {
+        await fetch(`${this.baseURL}/users/${id}`, {
+            method: "DELETE",
+        });
+    }
+
+    public async getQuestions(): Promise<Array<Question>> {
+        const response = await fetch(`${this.baseURL}/questions`);
+        return await response.json();
+    }
+
+    public async createQuestion(question: Question): Promise<Question> {
+        const response = await fetch(`${this.baseURL}/questions`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(question),
+        });
+        return await response.json();
+    }
+
+    public async updateQuestion(question: Question): Promise<Question> {
+        const response = await fetch(`${this.baseURL}/questions/${question.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(question),
+        });
+        return await response.json();
+    }
+
+    public async deleteQuestion(id: number): Promise<void> {
+        await fetch(`${this.baseURL}/questions/${id}`, {
+            method: "DELETE",
+        });
+    }
+
+    // public async getQuestionsByUser(id: number): Promise<Array<Question>> {
+    //     const response = await fetch(`${this.baseURL}/users/${id}/questions`);
+    //     return await response.json();
+    // }
+
+    // public async getQuestionsByUserAndTag(id: number, tag: string): Promise<Array<Question>> {
+    //     const response = await fetch(`${this.baseURL}/users/${id}/questions/${tag}`);
+    //     return await response.json();
+    // }
+}
