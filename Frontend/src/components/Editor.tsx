f39c00a65149d5100d1a251ee0a6cc72b23585b7
Gibson <gibsonyip@u.nus.edu>
2023-10-11 17:40:53 +0800

    Completed collaborative code editor v1
    Basic syntax is supported for all languages, auto-completion varies across languages. Will work on this soon.

--- Frontend/src/components/Editor.tsx
+++ Frontend/src/components/Editor.tsx
@@ -2,8 +2,8 @@ import { useState, useRef } from 'react'
 import Editor from "@monaco-editor/react"
 import * as Y from "yjs"
 import { WebrtcProvider } from "y-webrtc"
-import { MonacoBinding } from "../libs/y-monaco"
-import { Select } from '@chakra-ui/react'
+import { MonacoBinding } from "../lib/y-monaco"
+import {Container, Select, Button, Box} from "@chakra-ui/react"
 
 // Setup Monaco Editor
 // Attach YJS Text to Monaco Editor
@@ -11,19 +11,22 @@ import { Select } from '@chakra-ui/react'
 function CodeEditor() {
 
   const editorRef = useRef(null);
-  const [lang, setLang] = useState("javascript");
+  const [lang, setLang] = useState("python");
+  const [loading, setLoading] = useState(false);
 
   // Editor value -> YJS Text value (A text value shared by multiple people)
   // One person deletes text -> Deletes from the overall shared text value
   // Handled by YJS
 
   // Initialize YJS, tell it to listen to our Monaco instance for changes.
-
 function handleEditorDidMount(editor:any, monaco: any) {
   editorRef.current = editor;
   // Initialize YJS
   const doc = new Y.Doc(); // a collection of shared objects -> Text
-  // Connect to peers (or start connection) with WebRTC
+  /* Connect to peers (or start connection) with WebRTC
+  // have to generate a unique sessionID during matching so that matched
+  users can have a shared room to code
+  */
   const provider = new WebrtcProvider("test-room", doc); // room1, room2
   const type = doc.getText("monaco"); // doc { "monaco": "what our IDE is showing" }
   // Bind YJS to Monaco 
@@ -32,6 +35,63 @@ function handleEditorDidMount(editor:any, monaco: any) {
   console.log(provider.awareness);                
 }
 
+function submitCode() {
+  //@ts-ignore
+  const data= editorRef.current.getValue()
+  alert(data)
+  setLoading(true)
+}
+
+function changeLang(lang:any) {
+  setLang(lang)
+}
+
+/*will have to modify this function to identify what
+is the default language specified by the user and
+add selected property to the other options
+*/
+function isSelected() {
+  return true;
+}
+
+return (
+  <div>
+    <Container mb={16} style={{ position: 'relative', height: '50vh' }}>
+      <div style={{ float:'right', paddingRight:3}}>
+        <Select placeholder='Select Language' size={'md'} onChange={(e : React.ChangeEvent<HTMLSelectElement>) => {changeLang(e.target.value)}}>
+          <option value='csharp'>C#</option> 
+          <option value='cpp'>C++</option>
+          <option value='python' selected = {isSelected()}>Python</option>
+          <option value='go'>Go</option>
+          <option value='java'>Java</option>
+          <option value='kotlin'>Kotlin</option>
+        </Select>
+      </div>
+    <Editor
+      height="50vh"
+      width="100vw"
+      theme="vs-dark"
+      language ={lang}
+      onMount={handleEditorDidMount}/>
+      <div style={{ float:'right', paddingRight:5}}>
+        <Button
+          isLoading={loading}
+          onClick={submitCode}
+          loadingText='Submitting'
+          colorScheme='teal'
+          variant='outline'>
+          Submit Code
+        </Button>
+      </div>
+      
+    </Container>
+  </div>
+  )
+}
+
+export default CodeEditor
+
+/*
 function handleEditorWillMount(monaco : any) {
   monaco.languages.registerCompletionItemProvider('python', {
     provideCompletionItems: function(model : any, position : any) {
@@ -60,38 +120,4 @@ function createDependencyProposals(range: any) {
       },
   ];
 }
-
-  function submitCode() {
-    //@ts-ignore
-    const data= editorRef.current.getValue()
-    alert(data)
-    
-  }
-
-  function changeLang() {
-    setLang("python")
-  }
-
-  return (
-    <div>
-      <Editor
-      height="50vh"
-      width="50vw"
-      theme="vs-dark"
-      language ={lang}
-      beforeMount={handleEditorWillMount}
-      onMount={handleEditorDidMount}/>
-      <Select placeholder='Select option'>
-        <option value='C#'>Option 1</option>
-        <option value='C++'>Option 2</option>
-        <option value='Python'>Option 3</option>
-        <option value='Java'>Option 3</option>
-      </Select>
-      <button onClick={changeLang}>Change language!</button>
-      <button onClick={submitCode}>Submit!</button>
-    </div>
-    
-  )
-}
-
-export default CodeEditor
+*/
