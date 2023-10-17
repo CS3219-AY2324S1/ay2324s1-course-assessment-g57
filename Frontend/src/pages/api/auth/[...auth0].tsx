b052057adfb643cec7d5c80d25867f5b3c224f99
Owen Tan <owen.tan@synapxe.sg>
2023-10-18 07:41:12 +0800

    Add CDK

--- Frontend/src/pages/api/auth/[...auth0].tsx
+++ Frontend/src/pages/api/auth/[...auth0].tsx
@@ -1,5 +1,6 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0'
 
+console.log('the AUTH0_SECRET env var is set: ', process.env.AUTH0_SECRET);
 export default handleAuth({
   async login(req : any, res: any) {
     try {
