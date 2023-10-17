193b8f6d29b36629443cfeca2aa79abf5685f8ee
Gibson <gibsonyip@u.nus.edu>
2023-10-17 10:54:26 +0800

    Updated UI and Editor's awareness

--- Frontend/src/components/Editor.tsx
+++ Frontend/src/components/Editor.tsx
@@ -3,8 +3,12 @@ import Editor from "@monaco-editor/react"
 import * as Y from "yjs"
 import { WebrtcProvider } from "y-webrtc"
 import { MonacoBinding } from "../lib/y-monaco"
-import {Button, Card, CardBody, Select, Switch} from "@chakra-ui/react"
+import {Button, Card, CardBody, Select, Switch, IconButton} from "@chakra-ui/react"
 import { fromUint8Array, toUint8Array } from 'js-base64'
+
+import * as random from 'lib0/random'
+
+import { MdOutlineDarkMode } from 'react-icons/md';
 
 // Setup Monaco Editor
 // Attach YJS Text to Monaco Editor
@@ -15,79 +19,108 @@ function CodeEditor() {
   const [loading, setLoading] = useState(false);
   const [editorTheme, setEditorTheme] = useState("light")
 
+  const usercolors = [
+    { color: '#30bced', light: '#30bced33' },
+    { color: '#6eeb83', light: '#6eeb8333' },
+    { color: '#ffbc42', light: '#ffbc4233' },
+    { color: '#ecd444', light: '#ecd44433' },
+    { color: '#ee6352', light: '#ee635233' },
+    { color: '#9ac2c9', light: '#9ac2c933' },
+    { color: '#8acb88', light: '#8acb8833' },
+    { color: '#1be7ff', light: '#1be7ff33' }
+  ]
+
+  // select a random color for this user
+  const userColor = usercolors[random.uint32() % usercolors.length]
+
   // Editor value -> YJS Text value (A text value shared by multiple people)
   // One person deletes text -> Deletes from the overall shared text value
   // Handled by YJS
 
   // Initialize YJS, tell it to listen to our Monaco instance for changes.
-function handleEditorDidMount(editor:any, monaco: any) {
-  editorRef.current = editor;
-  // Initialize YJS
-  const doc = new Y.Doc(); // a collection of shared objects -> Text
-  /* Connect to peers (or start connection) with WebRTC
-  // have to generate a unique sessionID during matching so that matched
-  users can have a shared room to code
+  function handleEditorDidMount(editor:any, monaco: any) {
+    editorRef.current = editor;
+    // Initialize YJS
+    const doc = new Y.Doc(); // a collection of shared objects -> Text
+    /* Connect to peers (or start connection) with WebRTC
+    // have to generate a unique sessionID during matching so that matched
+    users can have a shared room to code
+    */
+    const provider = new WebrtcProvider("test-room", doc); // room1, room2
+    //provider awareness for each user
+    provider.awareness.setLocalStateField('user', 
+    {name: 'Anonymous ' + Math.floor(Math.random() * 100),
+    color: userColor.color,
+    colorLight: userColor.light})
+
+    const type = doc.getText("monaco"); // doc { "monaco": "what our IDE is showing" }
+
+    // Bind YJS to Monaco 
+    //@ts-ignore
+    const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness);
+    console.log(provider.awareness);                
+  }
+
+
+  /*Save the code as a binary file*/
+  function submitCode() : string {
+    //@ts-ignore
+    const data= editorRef.current.getValue();
+    alert(data);
+    setLoading(true);
+    var enc = new TextEncoder(); 
+    var dec = new TextDecoder();
+    var documentState = enc.encode(data);
+    // Transform Uint8Array to a Base64-String
+    const base64Encoded = fromUint8Array(documentState);
+    alert(base64Encoded)
+    // Transform Base64-String back to an Uint8Array
+    const binaryEncoded = toUint8Array(base64Encoded);
+    alert(binaryEncoded);
+    //convert back to string
+    alert(dec.decode(binaryEncoded));
+
+    return base64Encoded;
+  }
+
+  function changeLang(lang:any) {
+    setLang(lang);
+  }
+
+  /*will have to modify this function to identify what
+  is the default language specified by the user and
+  add selected property to the other options
   */
-  const provider = new WebrtcProvider("test-room", doc); // room1, room2
-  const type = doc.getText("monaco"); // doc { "monaco": "what our IDE is showing" }
-  // Bind YJS to Monaco 
-  //@ts-ignore
-  const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness);
-  console.log(provider.awareness);                
+  function isSelected() {
+    return true;
+  }
 
-}
-/*Save the code as a binary file*/
-function submitCode() : string {
-  //@ts-ignore
-  const data= editorRef.current.getValue();
-  alert(data);
-  setLoading(true);
-  var enc = new TextEncoder(); 
-  var dec = new TextDecoder();
-  var documentState = enc.encode(data);
-  // Transform Uint8Array to a Base64-String
-  const base64Encoded = fromUint8Array(documentState);
-  alert(base64Encoded)
-  // Transform Base64-String back to an Uint8Array
-  const binaryEncoded = toUint8Array(base64Encoded);
-  alert(binaryEncoded);
-  //convert back to string
-  alert(dec.decode(binaryEncoded));
-
-  return base64Encoded;
-}
-
-function changeLang(lang:any) {
-  setLang(lang);
-}
-
-/*will have to modify this function to identify what
-is the default language specified by the user and
-add selected property to the other options
-*/
-function isSelected() {
-  return true;
-}
-
-function setDarkMode() {
-  editorTheme == "light" ? setEditorTheme("vs-dark") : setEditorTheme("light")
-}
+  function setDarkMode() {
+    editorTheme == "light" ? setEditorTheme("vs-dark") : setEditorTheme("light")
+  }
 
 return (
   <div>
     <Card style={{ display: 'flex', flexDirection: 'column', height: '80vh' }}>
       <CardBody>
         <Editor
+          height={"100%"}
+          width={"100%"}
           theme={editorTheme}
           language={lang}
           onMount={handleEditorDidMount}
-          options={{fontSize: 12}}
+          options={{fontSize: 12, automaticLayout: true}}
         />
       </CardBody>
     </Card>
     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: 5 }}>
       <div style={{ display: 'flex', alignItems: 'center', paddingLeft:2}}>
-        Dark Mode?
+        <IconButton aria-label='Dark Mode' 
+          variant='outline' 
+          colorScheme='white' 
+          fontSize='20px' 
+          height={30}
+          icon={<MdOutlineDarkMode/>} />
         <Switch onChange={setDarkMode} paddingLeft={2} paddingRight={5}/>
       </div>
 
