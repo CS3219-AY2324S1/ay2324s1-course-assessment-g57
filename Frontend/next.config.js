b9567516dbf235a0a6f68b7257775609c4da845f
Owen Tan <owen.tan@synapxe.sg>
2023-10-18 09:21:50 +0800

    add env

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -2,12 +2,12 @@
  const nextConfig = { 
   reactStrictMode: true,
     // Can be safely removed in newer versions of Next.js
-  future: {
+  // future: {
 
     // by default, if you customize webpack config, they switch back to version 4.
     // Looks like backward compatibility approach.
-    webpack5: true,   
-  },
+  //   webpack5: true,   
+  // },
 
   webpack(config) {
     config.resolve.fallback = {
@@ -20,6 +20,14 @@
     };
     
     return config;
+  },
+  env: {
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    SERVER_URL: process.env.SERVER_URL
   }
 }
 
