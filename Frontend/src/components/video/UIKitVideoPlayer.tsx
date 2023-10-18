ebb8155a44d2cf117baae115d816d1780eac16ea
Daniel Lim <DanielLimWeiEn@gmail.com>
2023-10-18 16:49:34 +0800

    Remove unnecessary components and update styling for UIKitVideoPlayer

--- Frontend/src/components/video/UIKitVideoPlayer.tsx
+++ Frontend/src/components/video/UIKitVideoPlayer.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import AgoraUIKit from "agora-react-uikit";
+import AgoraUIKit, { layout } from "agora-react-uikit";
 import axios from "axios";
+import { Button } from "@chakra-ui/react";
 
 const SERVER_URL = process.env.SERVER3_URL || "http://localhost:3500";
 
@@ -39,6 +40,40 @@ const UIKitVideo = ({ channel }: { channel: string }) => {
     channel: channel,
     token: token,
     disableRtm: true,
+    layout: layout.grid,
+  };
+
+  const styleProps = {
+    BtnTemplateStyles: {}, // Style the local buttons, except for end call button.
+    UIKitContainer: {}, // Style the global view containing AgoraUIKit.
+    gridVideoCalls: {}, // Style the individual cells containing the videos in the grid layout.
+    gridVideoContainer: {}, // Style the container storing the individual cells in grid layout.
+    iconSize: 13, // Customize size of the icons.
+    localBtnContainer: {
+      backgroundColor: "#B2BEB5",
+    }, // Style the container for the buttons.
+    localBtnStyles: {
+      muteLocalVideo: {
+        borderRadius: 25,
+        width: 40,
+        height: 40,
+        borderWidth: 1,
+      }, // Camera Button.
+      muteLocalAudio: {
+        borderRadius: 25,
+        width: 40,
+        height: 40,
+        borderWidth: 1,
+      }, // Mute Button.
+      endCall: {
+        borderRadius: 25,
+        width: 60,
+        height: 40,
+        backgroundColor: "#f66",
+        borderWidth: 1,
+      }, // End Call Button.
+    }, // Style for specific local buttons.
+    theme: "white", // Color tint for the icons in the buttons.
   };
 
   const callbacks = {
@@ -48,12 +83,27 @@ const UIKitVideo = ({ channel }: { channel: string }) => {
   return (
     <div>
       {videoCall && (
-        <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
-          <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+        <div style={{ display: "flex", width: "50vw", height: "50vh" }}>
+          <AgoraUIKit
+            rtcProps={rtcProps}
+            callbacks={callbacks}
+            styleProps={styleProps}
+          />
         </div>
       )}
       {!videoCall && (
-        <button onClick={() => setVideoCall(true)}>Start Call</button>
+        <Button
+          onClick={() => setVideoCall(true)}
+          style={{
+            borderRadius: 25,
+            width: 100,
+            height: 60,
+            backgroundColor: "white",
+            borderWidth: 1,
+          }}
+        >
+          Start Call
+        </Button>
       )}
     </div>
   );
--- Frontend/src/pages/video.tsx
+++ Frontend/src/pages/video.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import dynamic from "next/dynamic";
-import NavBar from "../components/nav";
+import NavBar from "../components/Nav";
 
 const UIKitVideoWithNoSSR = dynamic(
   () => import("../components/video/UIKitVideoPlayer"),
