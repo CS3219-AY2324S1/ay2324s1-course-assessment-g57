1a35afb048443a6328171503c2666a58592ccc0a
Daniel Lim <DanielLimWeiEn@gmail.com>
2023-10-31 23:14:32 +0800

    Update Login Test

--- Frontend/cypress/e2e/login_spec.cy.ts
+++ Frontend/cypress/e2e/login_spec.cy.ts
@@ -1,8 +1,12 @@
 describe('The Login Process', () => {
-    it('sets and auth cookie and when logging in via form submission', () => {
+    it('sets an auth cookie and when logging in via form submission', () => {
       cy.visit('')
 
+      cy.intercept('https://dev-r67hrnstb5x4ekjv.us.auth0.com/*').as("getAuth");
+
       cy.get("a[href*='auth']").click();
+
+      cy.wait("@getAuth");
 
       cy.origin('https://dev-r67hrnstb5x4ekjv.us.auth0.com/*', () => {
         cy.get("input[name=username]").type("danielTestUser");
@@ -13,6 +17,8 @@ describe('The Login Process', () => {
       cy.url().should('include', 'http://localhost:3000');
 
       cy.getCookie('appSession').should('exist');
+
+      cy.get("a[href*=logout]").click();
     })
-  })
+})
   
