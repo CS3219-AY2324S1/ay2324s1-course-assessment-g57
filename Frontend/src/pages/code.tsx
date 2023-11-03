df56ca4c279013a537a501f6ba8c15336d4127ca
He Shuimei <shuimeihe@gmail.com>
2023-11-04 01:11:42 +0800

    Revert code.tsx

--- Frontend/src/pages/code.tsx
+++ Frontend/src/pages/code.tsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
+import { useSearchParams } from 'next/navigation';
 import TopBar from '../components/collab/TopBar';
 import QuestionDisplay from '../components/collab/QuestionDisplay';
 import socket from '../lib/socket';
-import { RoomContext } from '@/contexts/RoomContext';
 
 const MonacoEditorComponentWithNoSSR = dynamic(
     () => import('../components/editor/Editor'),
@@ -17,18 +17,14 @@ const AgoraUIKit = dynamic(
 const CodeEditorPage = () => {
     const searchParams = useSearchParams();
     const [roomId, setRoomId] = useState(searchParams.get('room'));
-    const [currentQnId, setCurrentQnId] = useState<string>(
-        searchParams.get('qnId') || '0'
-    );
-    const [currDifficulty, setDifficulty] = useState(
-        searchParams.get('difficulty')
-    );
+    const [currentQnId, setCurrentQnId] = useState<string>(searchParams.get('qnId')||"0");
+    const [currDifficulty, setDifficulty] = useState(searchParams.get('difficulty'));
 
     socket.on('questionUpdate', (data) => {
-        const { roomId, qnId } = data;
+        const {roomId, qnId} = data;
         // console.log(`roomid ${roomId}`);
         // console.log(`updating id with: ${qnId}`);
-        setCurrentQnId(`${qnId}`);
+        setCurrentQnId(`${qnId}`)
         // console.log(`now : ${currentQnId}`);
     });
 
@@ -40,21 +36,22 @@ const CodeEditorPage = () => {
         // console.log(id);
 
         setRoomId(id);
-        setCurrentQnId(qnId || '0');
-        setDifficulty(difficulty);
+        setCurrentQnId(qnId||"0");
+        setDifficulty(difficulty)
 
-        socket.emit('questionUpdate', { roomId, difficulty });
+        socket.emit("questionUpdate", {roomId, difficulty});
     }, []);
 
     function getNewQn() {
-        socket.emit('questionUpdate', { roomId, difficulty: currDifficulty });
+        socket.emit("questionUpdate", {roomId, difficulty: currDifficulty});
     }
+
     return (
         <>
             <TopBar />
             <div className="columns">
                 <div className="column">
-                    <QuestionDisplay qnId={currentQnId} getNewQnFn={getNewQn} />
+                    <QuestionDisplay qnId={currentQnId} getNewQnFn={getNewQn}/>
                     <AgoraUIKit channel={roomId || ''} />
                 </div>
                 <div className="column is-three-fifths">
