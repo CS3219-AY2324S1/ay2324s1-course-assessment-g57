ea9dc3f6dca9ff9d23ac1484d9498d8d6f94721e
Daniel Lim <DanielLimWeiEn@gmail.com>
2023-11-09 13:26:33 +0800

    Update UIKitVideoPlayerSpec

--- Frontend/cypress/component/UIKitVideoPlayer_spec.cy.tsx
+++ Frontend/cypress/component/UIKitVideoPlayer_spec.cy.tsx
@@ -7,7 +7,7 @@ describe('Video Player Component Test', () => {
         cy.mount(<UIKitVideo channel="TestVideoChat" />);
 
         // // Check button to start the video call and start the call
-        // cy.get("button[class='button is-link'", { timeout: 10000 }).should("be.visible").click();
+        cy.get("button[class='button is-link'", { timeout: 10000 }).should("be.visible");
 
         // // Check the option to toggle video button
         // cy.get("svg[xlmns='http://www.w3.org/2000/svg']").should("be.visible").click();
