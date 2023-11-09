2abd7bbfb45b35ed1f99a52ef81a58ce9a315db9
Daniel Lim <DanielLimWeiEn@gmail.com>
2023-11-09 16:44:27 +0800

    Update Video Call Component Test to replace Video Call End to End Test

--- Frontend/cypress/component/UIKitVideoPlayer_spec.cy.tsx
+++ Frontend/cypress/component/UIKitVideoPlayer_spec.cy.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
 import UIKitVideo from '@/components/video/UIKitVideoPlayer';
-import CodeEditor from '@/components/editor/Editor';
+import { ChakraBaseProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({});
+
 describe('Video Player Component Test', () => {
     it('mounts', () => {
-        cy.mount(<CodeEditor roomId='test'/>);
+        cy.mount(
+            <ChakraBaseProvider theme={theme}>
+                <UIKitVideo channel="test" />
+            </ChakraBaseProvider>
+        );
 
-        // // Check button to start the video call and start the call
-        cy.get("button[class='button is-link'", { timeout: 10000 }).should("be.visible");
-
-        // // Check the option to toggle video button
-        // cy.get("svg[xlmns='http://www.w3.org/2000/svg']").should("be.visible").click();
+        // Check that the video service can be accessed from the collaboration page and that users
+        // can get a token for the video chat.
+        // cy.intercept(
+        //     'https://34k0nfj43f.execute-api.ap-southeast-1.amazonaws.com/dev/*'
+        // ).as('getToken');
+        cy.get("button[class='button is-link']", { timeout: 10000 }).should(
+            'be.visible'
+        );
+        // cy.wait('@getToken').then(
+        //     (intercept) => expect(intercept?.response).to.exist
+        // );
     });
 });
